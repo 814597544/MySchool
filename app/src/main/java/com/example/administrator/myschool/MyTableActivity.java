113e3397b50883acfdd2f55abe4d11c9926fb065
814597544 <814597544@qq.com>
2015-03-09 15:42:52 +0800

    table1

--- app/src/main/java/com/example/administrator/myschool/MyTableActivity.java
+++ app/src/main/java/com/example/administrator/myschool/MyTableActivity.java
@@ -2,6 +2,9 @@ package com.example.administrator.myschool;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
@@ -9,6 +12,10 @@ import android.widget.TextView;
  */
 public class MyTableActivity extends Activity{
     TextView title;
+    RelativeLayout my_table_list,update_my_table,add_my_table;
+    ImageView my_table__list_image0,my_table__list_image1,update_my_table_image0,update_my_table_image1,add_my_table_image;
+    TextView my_table_list_num,update_my_table_num;
+    ListView mytable_list,update_mytable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +26,18 @@ public class MyTableActivity extends Activity{
     private void findId() {
         title= (TextView) findViewById(R.id.title);
         title.setText("课 表");
+        my_table_list= (RelativeLayout) findViewById(R.id.my_table_list);
+        update_my_table= (RelativeLayout) findViewById(R.id.update_my_table);
+        add_my_table= (RelativeLayout) findViewById(R.id.add_my_table);
+        my_table__list_image0= (ImageView) findViewById(R.id.my_table__list_image0);
+        my_table__list_image1= (ImageView) findViewById(R.id.my_table__list_image1);
+        update_my_table_image0= (ImageView) findViewById(R.id.update_my_table_image0);
+        update_my_table_image1= (ImageView) findViewById(R.id.update_my_table_image1);
+        add_my_table_image= (ImageView) findViewById(R.id.add_my_table_image);
+        my_table_list_num= (TextView) findViewById(R.id.my_table_list_num);
+        update_my_table_num= (TextView) findViewById(R.id.update_my_table_num);
+        mytable_list= (ListView) findViewById(R.id.mytable_list);
+        update_mytable= (ListView) findViewById(R.id.update_mytable);
+
     }
 }
