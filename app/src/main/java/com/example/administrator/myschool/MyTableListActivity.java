f7dde65caf7b534ead798b8956e945306dd4b234
814597544 <814597544@qq.com>
2015-03-10 16:22:29 +0800

    table2

--- app/src/main/java/com/example/administrator/myschool/MyTableListActivity.java
+++ app/src/main/java/com/example/administrator/myschool/MyTableListActivity.java
@@ -15,7 +15,7 @@ import com.circlemenu.CircleLayout;
  * Created by Administrator on 2015/3/9.
  */
 public class MyTableListActivity extends Activity   implements CircleLayout.OnItemSelectedListener, CircleLayout.OnItemClickListener {
-    private TextView selectedTextView;
+    private TextView selectedTextView,title;
     private CircleLayout circleMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +28,17 @@ public class MyTableListActivity extends Activity   implements CircleLayout.OnIt
         circleMenu = (CircleLayout)findViewById(R.id.main_circle_layout);
 
         selectedTextView = (TextView)findViewById(R.id.main_selected_textView);
+        title = (TextView)findViewById(R.id.title);
+        circleMenu.setOnItemSelectedListener(this);
+        circleMenu.setOnItemClickListener(this);
+
         selectedTextView.setText(((CircleImageView)circleMenu.getSelectedItem()).getName());
+        title.setText("课表名称");
     }
 
     @Override
        public void onItemClick(View view, int position, long id, String name) {
-        Toast.makeText(getApplicationContext(), getResources().getString(R.string.action_settings) + " " + name, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), name, Toast.LENGTH_SHORT).show();
     }
 
     @Override
