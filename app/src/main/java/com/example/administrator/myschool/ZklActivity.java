bd9aa3d16a1d1e21b0503e26a22db50240d2cbcb
814597544 <814597544@qq.com>
2015-03-31 10:53:39 +0800

    zkl 4.1

--- app/src/main/java/com/example/administrator/myschool/ZklActivity.java
+++ app/src/main/java/com/example/administrator/myschool/ZklActivity.java
@@ -2,19 +2,24 @@ package com.example.administrator.myschool;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
@@ -23,7 +28,11 @@ import com.nineoldandroids.animation.Animator;
 import com.rao.MySchool.adapter.CircularPagerAdapter;
 import com.rao.MySchool.been.DatabaseHelper;
 import com.rao.MySchool.been.MyApplication;
+import com.rao.MySchool.service.BindService;
 import com.viewpagerindicator.CirclePageIndicator;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.nineoldandroids.animation.Animator.*;
 
@@ -34,10 +43,11 @@ public class ZklActivity extends Activity{
     private RoundCornerProgressBar progressTwo;
     private ImageView add;
 
-    private int progress2 = 5;
+    private int progress2 = 0;
     private CircularBarPager mCircularBarPager;
     TextView titleName,zkl_show_dream,zkl_show_wast,zkl_show_break;
-    String shownum = null;
+    String shownum = null,nowtime,MyDreamTime=null;
+    boolean MyBoolean=false;
 
     DatabaseHelper databaseHelper;
     SQLiteDatabase sqLiteDatabase;
@@ -47,6 +57,19 @@ public class ZklActivity extends Activity{
      * The animation time in milliseconds that we take to display the steps taken
      */
     private static final int BAR_ANIMATION_TIME = 1000;
+
+    BindService.MyBinder binder;
+    private ServiceConnection conn = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.i("zkl", "--Service Disconnected--");
+        }
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            // 取得Service对象中的Binder对象
+            binder = (BindService.MyBinder) service;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +105,7 @@ public class ZklActivity extends Activity{
                         myApplication.setDreamTime(cursor.getString(2));
                         myApplication.setBreakTime("11");
                         myApplication.setWastTime(13 - Integer.parseInt(cursor.getString(2)) + "");
+                        MyDreamTime=myApplication.getDreamTime();
                         zkl_show_dream.setText(myApplication.getDreamTime() + "小时");
                         zkl_show_break.setText(myApplication.getBreakTime() + "小时");
                         zkl_show_wast.setText(myApplication.getWastTime() + "小时");
@@ -170,6 +194,29 @@ public class ZklActivity extends Activity{
 
 
     private void updateProgressTwo() {
+        getNowTime();
+        Cursor cursor3 = sqLiteDatabase.rawQuery("select time from mystatus where date=?;",new String[]{nowtime});
+
+        if (cursor3.getCount()==0){
+            try{
+                Log.e("111111","========"+Integer.parseInt(MyDreamTime));
+                progress2=binder.getCount()/(3600*Integer.parseInt(MyDreamTime));
+                Log.e("22222","2222222222222"+progress2);
+            }catch (Exception e){}
+
+        }else{
+            while (cursor3.moveToNext()){
+                try {
+                    progress2=(Integer.parseInt(cursor3.getString(0))+binder.getCount())/(3600*Integer.parseInt(myApplication.getDreamTime()));
+                             /* ------发送广播------*/
+                    Log.e("$$$$$$$","progress2="+progress2);
+                    Intent intent2 = new Intent();
+                    intent2.setAction("com.rao.myproject.Status");
+                    sendBroadcast(intent2);
+                }catch (Exception e){}
+            }
+        }
+
         progressTwo.setProgress(progress2);
         updateProgressTwoColor();
 
@@ -187,7 +234,8 @@ public class ZklActivity extends Activity{
     @Override
     protected void onResume() {
         super.onResume();
-        mCircularBarPager.getCircularBar().animateProgress(0, 50, 1000);
+        mCircularBarPager.getCircularBar().animateProgress(0, 0, 1000);
+
     }
 
     private void initViews(){
@@ -207,6 +255,53 @@ public class ZklActivity extends Activity{
         circlePageIndicator.setStrokeColor(getResources().getColor(R.color.transparent));
 
         //Do stuff based on animation
+       /*----------开始和停止梦想--------*/
+        mCircularBarPager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "zkl", Toast.LENGTH_SHORT).show();
+                getNowTime();
+
+                if (MyBoolean==false){
+                    final Intent intentg = new Intent();
+                    // 指定开启服务的action
+                    intentg.setAction("com.bgxt.BindServiceDemo");
+                    // 绑定服务到当前activity中
+                    bindService(intentg, conn, Service.BIND_AUTO_CREATE);
+                    MyBoolean=true;
+
+                }else{
+                    Cursor cursor2 = sqLiteDatabase.rawQuery("select date from mystatus ;",null);
+                    while (cursor2.moveToNext()) {
+                        if ( cursor2.getString(0).equals(nowtime)){
+                            binder=null;
+                            unbindService(conn);
+
+                 sqLiteDatabase.execSQL("update mystatus set  time =? where date=? ;", new String[]{""+binder.getCount(),nowtime});
+                            MyBoolean=false;
+                            break;
+                        }else{
+                            // 解除绑定
+                            binder=null;
+                            unbindService(conn);
+
+                            sqLiteDatabase.execSQL("insert into mystatus(date,time)  values(?,?);",
+                                    new Object[]{nowtime,""+binder.getCount()});
+
+                            MyBoolean=false;
+                            break;
+                        }
+
+                    }
+                }
+
+
+            }
+        });
+
+
+
         mCircularBarPager.addListener(new AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
@@ -216,6 +311,7 @@ public class ZklActivity extends Activity{
             @Override
             public void onAnimationEnd(Animator animation) {
                 //TODO do stuff
+
             }
 
             @Override
@@ -253,6 +349,7 @@ public class ZklActivity extends Activity{
     public class MyReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(final Context context, Intent intent) {
+            progressTwo.setProgress(progress2);
             shownum=myApplication.getStatus();
             zkl_show_dream.setText(myApplication.getDreamTime()+"小时");
             zkl_show_break.setText(myApplication.getBreakTime()+"小时");
@@ -316,4 +413,11 @@ public class ZklActivity extends Activity{
        }
     }
 }
+
+    public void getNowTime() {
+        SimpleDateFormat formatter   =   new   SimpleDateFormat   ("yyyy-MM-dd");
+        Date curDate   =   new   Date(System.currentTimeMillis());//获取当前时间
+        nowtime   =   formatter.format(curDate);
+    }
+
 }
---
+++ app/src/main/java/com/rao/MySchool/service/BindService.java
@@ -0,0 +1,71 @@
+package com.rao.MySchool.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class BindService extends Service {
+	private final static String TAG = "main";
+	private int count;
+	private boolean quit;
+	
+	private Thread thread;
+	private MyBinder binder=new MyBinder();
+	// ����һ��Binder��
+	public class MyBinder extends Binder
+	{
+		// ����һ����������count��¶���ⲿ����
+		public int getCount(){
+			return count;
+		}
+	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Log.i(TAG, "Service is Created");
+		thread=new Thread(new Runnable() {			
+			@Override
+			public void run() {
+				// ÿ���һ��count��1 ��ֱ��quitΪtrue��
+				while(!quit){
+					try{
+						Thread.sleep(1000);
+					}catch(InterruptedException e){
+						e.printStackTrace();
+					}
+					count++;
+				}
+			}
+		});
+		thread.start();
+	}
+	
+	@Override
+	public boolean onUnbind(Intent intent) {
+		Log.i(TAG, "Service is Unbinded");
+		return true;
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.i(TAG, "Service is started");
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.i(TAG, "Service is Destroyed");
+		this.quit=true;
+		
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.i(TAG, "Service is Binded");
+		return binder;
+	}
+}
